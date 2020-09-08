c4460d5813660ca157e05f4efeb03330ce8cf0e6
Delong90 <matushkin030790@gmail.com>
2020-09-08 07:24:52 +0300

    Home work / lesson03-04 / Task01-03

--- BH-J24/src/by/belhard/j24/HomeWork/Self/Lesson03_04/Task01.java
+++ BH-J24/src/by/belhard/j24/HomeWork/Self/Lesson03_04/Task01.java
@@ -27,22 +27,21 @@ public class Task01 {
             array[i] = in.nextInt();
             sum += array[i];
             list.add(array[i]);
+
         }
         average = sum/10;
 
         Collections.sort(list);
-        System.out.println("Сумма всех чисел массива равна: " + list.size());
 
-       // median = list.get(4);
-
-      //  median = (list.get((list.size()/2.0)) + list.get((list.size()/2.0) - 1)) / 2.0;
-       // System.out.println((list.get(4)+list.get(5))/2);
+        double a = (int) list.get(4);
+        double b = (int) list.get(5);
+        median = (a+b)/2;
 
         System.out.println("Сумма всех чисел массива равна: " + sum);
         System.out.println("Среднее арифметическое всех чисел массива равно: " + average);
 
         System.out.println("Отсортированные числа массива от минимального к максимальному: " + list);
-        //System.out.println("Медиана массива равна: " + median);
+        System.out.println("Медиана массива равна: " + median);
 
     }
 
